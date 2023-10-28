573e44c582b09d0ec726d955fec15d62e802bc56
Daisy Wang <daisyw2003@berkeley.edu>
2023-10-27 19:33:16 -0700

    test

---
+++ calhacks2023/pages/chatapp.py
@@ -0,0 +1,17 @@
+import reflex as rx
+
+@rxpage(route="/", title="ChatApp")
+
+def chatapp() -> rx.Component:
+    return rx.container(
+        rx.box(
+            "What is Reflex?",
+            # The user's question is on the right.
+            text_align="right",
+        ),
+        rx.box(
+            "A way to build web apps in pure Python!",
+            # The answer is on the left.
+            text_align="left",
+        ),
+    )
