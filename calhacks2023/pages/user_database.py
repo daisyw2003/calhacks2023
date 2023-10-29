33f399a8610114d4c041b1e5bdf9c10e84645ae9
Daisy Wang <daisyw2003@berkeley.edu>
2023-10-28 17:13:06 -0700

    ok so like the data table display works

--- calhacks2023/pages/user_database.py
+++ calhacks2023/pages/user_database.py
@@ -5,29 +5,23 @@ import reflex as rx
 from calhacks2023.state import State
 import pandas as pd
 
-dat = pd.read_csv("calhacks2023/pages/data.csv")
+
 
 class FormState(State):
 
     form_data: dict = {}
-
+    output = pd.DataFrame({'Name':[], 'Email':[]})
+    dat = pd.read_csv("calhacks2023/pages/data.csv")
     def handle_submit(self, form_data: dict):
         """Handle the form submit."""
         self.form_data = form_data
-        dat = pd.read_csv("calhacks2023/pages/data.csv")
-        if self.form_data["get"] == False:
-            new_record = pd.DataFrame([{'Name':form_data["name"], 'Email':form_data["email"]}])
-            dat = pd.concat([dat, new_record], ignore_index= True)
-            dat.to_csv("calhacks2023/pages/data.csv")
-
-
-
-class CheckboxState(State):
-    checked: bool = False
-
-    def toggle(self):
-        self.checked = not self.checked
-
+        if self.form_data["get"]:
+            self.output = self.dat.loc[self.dat["Name"] == self.form_data["name"]]
+        else:
+            new_record = pd.DataFrame([{'Name':self.form_data["name"], 'Email':self.form_data["email"]}])
+            self.dat = pd.concat([self.dat, new_record], ignore_index=True)
+            self.dat.to_csv("calhacks2023/pages/data.csv")
+            self.output = self.dat.loc[self.dat["Name"] == self.form_data["name"]]
 
 
 
@@ -43,19 +37,29 @@ def user_database():
                 rx.input(
                     placeholder="Email", id="email"
                 ),
+                rx.input(
+                    placeholder="Task", id="task"
+                ),
+                rx.hstack(rx.input(
+                    placeholder="Date", id="date"
+                    ),
+                    rx.input(
+                        placeholder="Time Start", id="start"
+                    ),
+                    rx.input(
+                        placeholder="Time End", id="end"
+                    )
+                ),
                 rx.hstack(
-                    rx.checkbox("Get Data", on_change=CheckboxState.set_checked, id="get"),
+                    rx.checkbox("Clicking This Won't Add More Data", id="get"),
                 ),
                 rx.button("Submit", type_="submit"),
             ),
             on_submit=FormState.handle_submit,
         ),
         rx.divider(),
-        rx.cond(FormState.form_data["get"],
-                rx.table(data = dat.loc[dat["Name"] == FormState.form_data["name"]],
+        rx.data_table(data = FormState.output,
                         pagination=True,
                         search=True,
                         sort=True),
-                rx.text("ADDED")
-                )
     )
